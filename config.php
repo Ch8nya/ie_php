cf64e615e19be919e6e8aa1e1c2be3ba442b5489
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-04 01:51:10 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+$dbUsername = 'cux5nmdarh8rqgpx';
+$dbPassword = 'zjd0gozcfoirbp2a';
+$dbName = 'laonzmp2o0pw3k5c';
+$port = '3306';
+
+
+$connect= mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_name) or die("database connection error");
