7121621d9acb410b7dfa1c1d333da1a6984f5843
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-12 20:59:51 +0530

    Update learn.php

--- learn.php
+++ learn.php
@@ -5,18 +5,6 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
-
-$user_id = $_SESSION['user_id'];
-$conn = new mysqli("onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "cux5nmdarh8rqgpx", "zjd0gozcfoirbp2a", "laonzmp2o0pw3k5c", 3306);
-$sql = "SELECT buy_status FROM users WHERE id = $user_id";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-$buy_status = $row['buy_status'];
-
-if ($buy_status != 1) {
-    header("Location: buy.php");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
