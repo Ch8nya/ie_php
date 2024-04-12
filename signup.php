f7899db65b0ba98a38262f3b01f289aa799bb4e5
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-12 20:58:27 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
@@ -10,9 +10,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Sign up successful. <a href='login.php'>Login here</a>";
+        $success = "Sign up successful. Login <a href='login.php'>here</a>";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        $error = "Error: " . $sql . "<br>" . $conn->error;
     }
 }
 ?>
@@ -24,20 +24,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h2>Sign Up</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <?php if (isset($error)) { echo "<p>$error</p>"; } ?>
+    <?php if (isset($success)) { echo "<p>$success</p>"; } else { ?>
+    <form method="POST" action="">
         <label>First Name:</label>
         <input type="text" name="first_name" required><br>
-        
         <label>Last Name:</label>
         <input type="text" name="last_name" required><br>
-        
         <label>Email:</label>
         <input type="email" name="email" required><br>
-        
         <label>Password:</label>
         <input type="password" name="password" required><br>
-        
         <input type="submit" value="Sign Up">
     </form>
+    <?php } ?>
 </body>
 </html>
