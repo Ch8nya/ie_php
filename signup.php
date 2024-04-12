6d070481e575d6d10b046d412bbcf563e962c97a
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-12 20:18:54 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,11 +1,12 @@
 <?php
+require_once 'config.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $conn = new mysqli("onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "cux5nmdarh8rqgpx", "zjd0gozcfoirbp2a", "laonzmp2o0pw3k5c", 3306);
     $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
 
     if ($conn->query($sql) === TRUE) {
