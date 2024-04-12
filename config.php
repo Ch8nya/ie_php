1fe4f236fd6d55f2d226d44d0a7f85d4132d04b9
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-12 20:56:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,13 @@
 <?php
-
 $host = 'onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-$dbUsername = 'cux5nmdarh8rqgpx';
-$dbPassword = 'zjd0gozcfoirbp2a';
-$dbName = 'laonzmp2o0pw3k5c';
-$port = '3306';
+$username = 'cux5nmdarh8rqgpx';
+$password = 'zjd0gozcfoirbp2a';
+$database = 'laonzmp2o0pw3k5c';
+$port = 3306;
 
+$conn = new mysqli($host, $username, $password, $database, $port);
 
-$connect= mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_name) or die("database connection error");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
