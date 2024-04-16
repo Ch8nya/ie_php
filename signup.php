1bde538159cee6238ad54711b60bdd76939ffa91
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2024-04-16 18:53:38 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -109,7 +109,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
         </div>
     </div>
+    <script>
+    // Assuming you have a form with id="signupForm"
+    document.getElementById("email-form").onsubmit = function(event) {
+        // Prevent the form from submitting immediately
+        event.preventDefault();
 
+        // Display the popup
+        document.getElementById("successPopup").style.display = "block";
+
+        // Redirect after 3 seconds
+        setTimeout(function() {
+            window.location.href = "login.php";
+        }, 3000);
+    };
+</script>
     <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=660eaa71aa6222fb22563027" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="script.js" type="text/javascript"></script>
 </body>
