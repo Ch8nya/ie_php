1e4a8efa70a75cbd09a3e707e0a59be3174c87a0
Rudraksha M J <143385610+rmj2626@users.noreply.github.com>
2024-06-01 17:39:59 +0530

    Revert

--- learn.php
+++ learn.php
@@ -153,7 +153,7 @@ if ($buy_status != 1) {
             <h1 class="heading-2">Module Title</h1>
             <h1 class="sub-heading">Lesson Title</h1>
             <div class="content"></div>
-                <a href="#" id="next-button"
+                <a href="#"
                 class="button-3 w-button">Next</a>
         </div>
         <div class="module-navigator">
@@ -402,14 +402,6 @@ function loadNextLesson() {
       const lessonTitle = titles.modules[currentModule - 1].lessons[currentLesson - 1];
       document.querySelector('.heading-2').textContent = moduleTitle;
       document.querySelector('.sub-heading').textContent = lessonTitle;
-
-      // Check if moduleNumber is 7 and lessonNumber is 2, then hide next-button
-      if (currentModule === 7 && currentLesson === 2) {
-        const nextButton = document.getElementById('next-button');
-        if (nextButton) {
-          nextButton.style.display = 'none';
-        }
-      }
     })
     .catch(error => {
       console.error('Error loading progress:', error);
@@ -417,7 +409,6 @@ function loadNextLesson() {
       loadLesson(currentModule, currentLesson);
     });
 });
-
 
 // Updated function to handle menu clicks
 function handleMenuClick(event) {
