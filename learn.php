a30adf4ce24605261eb36c9d8fa65b55ffa9cc0b
Rudraksha M J <143385610+rmj2626@users.noreply.github.com>
2024-06-08 21:06:55 +0530

    Update learn.php

--- learn.php
+++ learn.php
@@ -20,9 +20,7 @@ if ($buy_status != 1) {
 }
 ?>
 <!DOCTYPE html>
-<!-- This site was created in Webflow. https://www.webflow.com --><!-- Last Published: Tue Apr 16 2024 10:48:33 GMT+0000 (Coordinated Universal Time) -->
-<html data-wf-domain="ie-site-final.webflow.io" data-wf-page="660eaa71aa6222fb22563105"
-    data-wf-site="660eaa71aa6222fb22563027" lang="en">
+<html lang="en">
 
 <head>
     <meta charset="utf-8" />
@@ -30,7 +28,6 @@ if ($buy_status != 1) {
     <meta content="Projects" property="og:title" />
     <meta content="Projects" property="twitter:title" />
     <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta content="Webflow" name="generator" />
     <link
         href="styles.css"
         rel="stylesheet" type="text/css" />
@@ -41,15 +38,16 @@ if ($buy_status != 1) {
         type="text/javascript">WebFont.load({ google: { families: ["Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"] } });</script>
     <script
         type="text/javascript">!function (o, c) { var n = c.documentElement, t = " w-mod-"; n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch") }(window, document);</script>
-    <link href="https://assets-global.website-files.com/img/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-    <link href="https://assets-global.website-files.com/img/webclip.png" rel="apple-touch-icon" />
+    <link href="assets/Favicon.png" rel="shortcut icon" type="image/x-icon" />
+    <link href="assets/Favicon.png" rel="apple-touch-icon" />
+    <link href="assets/Favicon.png" rel="icon" type="image/x-icon" />
 </head>
 
 <body>
     <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease"
         role="banner" class="navbar w-nav">
         <div class="loggnavigation-wrap"><a href="Program.html" class="logo-link w-nav-brand"><img
-                    src="https://assets-global.website-files.com/660eaa71aa6222fb22563027/66183e8fa5ff3d56423fffbc_Frame%201%20(1).png"
+                    src="assets/Logo2.png"
                     width="Auto" alt="" class="logo-image-2" /></a>
             <div class="menumob">
                 <nav role="navigation" class="navigation-items w-nav-menu">
